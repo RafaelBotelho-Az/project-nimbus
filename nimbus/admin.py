7583a1157a306f294339ee2802860ab045bf19da
Rafael Botelho <rafaelbotelho@live.com>
2024-09-20 22:22:51 -0300

    começando a estruturar o html e inserindo dados dinamicamente

--- nimbus/admin.py
+++ nimbus/admin.py
@@ -3,11 +3,11 @@ from nimbus import models
 
 @admin.register(models.Art)
 class ArtAdmin(admin.ModelAdmin):
-    ...
+    list_display = 'artist', 'title', 'image', 'created_date', 
 
 @admin.register(models.Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    ...
+    list_display = 'user', 'profile_image', 
 
 @admin.register(models.Tag)
 class TagAdmin(admin.ModelAdmin):
--- nimbus/views.py
+++ nimbus/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from nimbus.models import Art
+
+
 
 def index(request):
+    posts = Art.objects.all()
+
+    context = {
+        'posts': posts,
+    }
+
     return render(
         request,
         'nimbus/index.html',
+        context
     )
