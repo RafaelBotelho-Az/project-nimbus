992a0a548fe25db3c22525faca7c52bbe63d9d28
Rafael Botelho <rafaelbotelho@live.com>
2024-09-23 22:20:51 -0300

    criado as views post e createPost, criado o template para o formulário do createPost

--- nimbus/urls.py
+++ nimbus/urls.py
@@ -5,4 +5,14 @@ app_name = 'nimbus'
 
 urlpatterns = [
     path('', views.index, name='index'),
+
+    # posts
+    path('post/<int:post_id>/', views.post, name='post'),
+    path('post/create', views.createPost, name='create'),
+
+
+
+    # path('post/<int:post_id>/update', views.post, name='post'),
+    # path('post/<int:post_id>/delete', views.post, name='post'),
+
 ]
--- nimbus/views.py
+++ nimbus/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from typing import Any
+from django.shortcuts import render, get_object_or_404
+from django import forms
+from django.core.exceptions import ValidationError
 from nimbus.models import Art
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Art
+        fields = (
+            'title', 'description', 'image', 'tags',
+        )
+    
+    def clean(self):
+        cleaned_data = self.cleaned_data
+
+        self.add_error(
+            'title',
+            ValidationError(
+                'Mensagem de erro',
+                code='invalid'
+            )
+        )
+        return super().clean()
 
 
 
@@ -8,10 +30,54 @@ def index(request):
 
     context = {
         'posts': posts,
+        'title': 'Explorar',
     }
 
     return render(
         request,
         'nimbus/index.html',
+        context
+    )
+
+
+
+def post(request, post_id):
+    single_post = get_object_or_404(Art.objects, pk=post_id)
+    site_tile = f'{single_post.title}'
+
+    context = {
+        'posts': single_post,
+        'title': site_tile,
+    }
+
+    return render(
+        request,
+        'nimbus/post.html',
+        context
+    )
+
+
+
+def createPost(request):
+    if request.method == 'POST':
+        context = {
+            'form': PostForm(request.POST),
+            'title': 'Criar',
+            }
+
+        return render(
+            request,
+            'nimbus/create-post.html',
+            context
+        )
+
+    context = {
+        'form': PostForm(),
+        'title': 'Criar',
+    }
+
+    return render(
+        request,
+        'nimbus/create-post.html',
         context
     )
